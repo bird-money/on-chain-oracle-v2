2fa403579b67649e29810dc2744fc6f6d79b9938
timon#1213 <money.bird.mi@gmail.com>
2021-05-23 06:33:17 +0500

    deployed https://testnet.bscscan.com/address/0xb01303FAEb6a0f05862138F477cC0EDD83b34aFd#contracts

--- migrations/2_migrate_oracle.js
+++ migrations/2_migrate_oracle.js
@@ -1,47 +1,57 @@
-const BirdOracle = artifacts.require('BirdOracle');
-const BirdToken = artifacts.require('RedToken');
+const BirdOracle = artifacts.require("BirdOracle");
+const BirdToken = artifacts.require("RedToken");
 
 const localDeployScript = async (deployer) => {
-  console.log('Deploying to local blockchain');
+  console.log("Deploying to local blockchain");
   await deployer.deploy(BirdToken);
   await deployer.deploy(BirdOracle, BirdToken.address);
 };
 
 const kovanDeployScript = async (deployer) => {
-  console.log('Deploying to kovan');
-  const usdtAddress = '0xaA7C9d8544Dd696239D9a953De2fAF7AA2852e22';
+  console.log("Deploying to kovan");
+  const usdtAddress = "0xaA7C9d8544Dd696239D9a953De2fAF7AA2852e22";
+  await deployer.deploy(BirdOracle, usdtAddress);
+};
+
+const bscTestnetDeployScript = async (deployer) => {
+  console.log("Deploying to bscTestnet");
+  const usdtAddress = "0x10e0BC6EBCf6B07D2c8457C806AbCf8Fa0bDA2b7";
   await deployer.deploy(BirdOracle, usdtAddress);
 };
 
 const mainnetDeployScript = async (deployer) => {
-  console.log('Deploying to Mainnet');
-  const birdTokenAddress = '0x70401dFD142A16dC7031c56E862Fc88Cb9537Ce0';
+  console.log("Deploying to Mainnet");
+  const birdTokenAddress = "0x70401dFD142A16dC7031c56E862Fc88Cb9537Ce0";
   await deployer.deploy(BirdOracle, birdTokenAddress);
 };
 
 const defaultDeployScript = async (deployer) => {
-  console.log('Deploying to Mainnet');
-  const birdTokenAddress = '0x70401dFD142A16dC7031c56E862Fc88Cb9537Ce0';
+  console.log("Deploying to Default");
+  const birdTokenAddress = "0x70401dFD142A16dC7031c56E862Fc88Cb9537Ce0";
   await deployer.deploy(BirdOracle, birdTokenAddress);
 };
 
 module.exports = async (deployer, network) => {
   switch (network) {
-    case 'mainnet':
+    case "mainnet":
       mainnetDeployScript(deployer);
       break;
 
-    case 'kovan':
+    case "bscTestnet":
+      bscTestnetDeployScript(deployer);
+      break;
+
+    case "kovan":
       kovanDeployScript(deployer);
       break;
 
-    case 'development':
-    case 'develop':
+    case "development":
+    case "develop":
       localDeployScript(deployer);
       break;
 
     default:
-      console.log('default: ', network);
+      console.log("default: ", network);
       defaultDeployScript(deployer);
   }
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,72 +1,85 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const infuraKey = "8165c77d80d441ab86e573b151f62b8d";
-
-const fs = require("fs");
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+require("dotenv").config();
+const MNEMONIC = process.env.MNEMONIC;
+const token = process.env.INFURA_TOKEN;
+const etherscanKey = process.env.BSCSCAN_KEY;
 
 module.exports = {
   networks: {
+    develop: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 6721975,
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*", // Match any network id
+      network_id: "*",
+      gas: 6721975,
     },
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*", // Match any network id
-    },
-    mainnet: {
-      provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          `https://mainnet.infura.io/v3/${infuraKey}`
-        ),
-      network_id: 1, // Mainnet's id
-    },
-
-    rinkeby: {
-      provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          `https://rinkeby.infura.io/v3/${infuraKey}`
-        ),
-      network_id: 4, // Ropsten's id
-      skipDryRun: true,
-    },
-
-    ropsten: {
-      provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          `https://ropsten.infura.io/v3/${infuraKey}`
-        ),
-      network_id: 3, // Ropsten's id
-      skipDryRun: true,
-    },
-
-    kovan: {
-      provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          `https://kovan.infura.io/v3/${infuraKey}`
-        ),
-      network_id: 42,
-      skipDryRun: true,
-    },
-  },
-  compilers: {
-    solc: {
-      version: "0.6.12",
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    bscTestnet: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          "https://data-seed-prebsc-1-s1.binance.org:8545"
+        );
       },
-      evmVersion: "petersburg",
+      network_id: "97",
+    },
+
+    bscMainnet: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          "https://bsc-dataseed.binance.org"
+        );
+      },
+      network_id: "56",
+    },
+
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          "https://mainnet.infura.io/v3/" + token
+        );
+      },
+      network_id: "1",
+    },
+    ropsten: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          "https://ropsten.infura.io/v3/" + token
+        );
+      },
+      network_id: "3",
+    },
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          "https://kovan.infura.io/v3/" + token
+        );
+      },
+      network_id: "42",
+      skipDryRun: true,
     },
   },
   plugins: ["truffle-plugin-verify"],
   api_keys: {
-    etherscan: "8R621NFWB6T6RMADTAZT8UGBBC5IATEUEM",
+    etherscan: etherscanKey,
+  },
+  compilers: {
+    solc: {
+      version: "0.6.12",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
   },
 };
